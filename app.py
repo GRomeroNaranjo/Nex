46b2d4d63087782a74d2a05e05d16506db7ff4b3
Guillem Romero Naranjo <143552937+GRomeroNaranjo@users.noreply.github.com>
2024-12-31 12:00:51 +0100

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from conversation import start_conversation
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-model_name = "meta-llama/Llama-2-7b-chat-hf"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
-
-start_conversation("Nex", "Guillem", model, tokenizer)
-
